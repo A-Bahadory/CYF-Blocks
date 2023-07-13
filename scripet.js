8fdc4bc3ef8a69e420cb42d471630c4195da68db
bahador <noor.bahadory786@gmail.com>
2023-07-13 23:33:37 +0100

    iput and clicks: say something

--- scripet.js
+++ scripet.js
@@ -84,3 +84,25 @@ document.getElementById('remove').addEventListener('click', (event) => {
   element_add_items2.replaceChildren();
 
 });
+//n7
+function convertToNumber(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return 0
+  } else {
+    return number_value
+  }
+}
+
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  window.speechSynthesis.speak(new SpeechSynthesisUtterance(document.getElementById('text').value));
+
+});
+
+document.getElementById('button').addEventListener('click', (event) => {
+  window.speechSynthesis.speak(new SpeechSynthesisUtterance(document.getElementById('cat').value));
+
+});
