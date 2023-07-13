b72fb4556a27adcbe56febdfe747ffdeede42e86
bahador <noor.bahadory786@gmail.com>
2023-07-13 23:06:47 +0100

    modifyin html dynamically: setting colour git push
    git push origin main
    git push roigin main
    git status
    ls -la
    cd
    git clear
    clear

--- scripet.js
+++ scripet.js
@@ -1,2 +1,14 @@
+//nu2
 let element_adjective = document.getElementById('adjective');
 element_adjective.innerText = 'big';
+
+//n3
+let element_banana = document.getElementById('banana');
+element_banana.style.color = '#ffff66';
+element_banana.style.backgroundColor = '#ff0000';
+let element_apple = document.getElementById('apple');
+element_apple.style.color = '#33cc00';
+element_apple.style.backgroundColor = '#3366ff';
+let element_strawberry = document.getElementById('strawberry');
+element_strawberry.style.color = '#ff0000';
+element_strawberry.style.backgroundColor = '#ff99ff';
