cc1b33ec488a74fa4e372f3bf4613fd53f75b232
bahador <noor.bahadory786@gmail.com>
2023-07-13 23:40:15 +0100

    button, inputs and clicks: a todo list

--- scripet.js
+++ scripet.js
@@ -106,3 +106,35 @@ document.getElementById('button').addEventListener('click', (event) => {
   window.speechSynthesis.speak(new SpeechSynthesisUtterance(document.getElementById('cat').value));
 
 });
+//8
+function convertToNumber(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return 0
+  } else {
+    return number_value
+  }
+}
+
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  let element_list = document.getElementById('list');
+  let new_li = document.createElement('li');
+  new_li.innerText = document.getElementById('text').value;
+
+  element_list.appendChild(new_li);
+
+});
+
+document.getElementById('new_button').addEventListener('click', (event) => {
+  let element_step_list = document.getElementById('step_list');
+  let new_li2 = document.createElement('li');
+  new_li2.innerText = '';
+
+  element_step_list.appendChild(new_li2);
+
+});
+document.getElementById('steps').value;
+
