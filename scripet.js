30b87ed8a38974fe15e0c2e31b3bc3affbe07207
bahador <noor.bahadory786@gmail.com>
2023-07-13 23:12:56 +0100

    creating html dynamically: list of fruit4

--- scripet.js
+++ scripet.js
@@ -12,3 +12,18 @@ element_apple.style.backgroundColor = '#3366ff';
 let element_strawberry = document.getElementById('strawberry');
 element_strawberry.style.color = '#ff0000';
 element_strawberry.style.backgroundColor = '#ff99ff';
+
+//n4
+let element_list = document.getElementById('list');
+let new_li = document.createElement('li');
+new_li.innerText = 'banana';
+
+element_list.appendChild(new_li);
+let new_li2 = document.createElement('li');
+new_li2.innerText = 'cat-fruit';
+
+element_list.appendChild(new_li2);
+let new_li3 = document.createElement('li');
+new_li3.innerText = 'avocado';
+
+element_list.appendChild(new_li3);
